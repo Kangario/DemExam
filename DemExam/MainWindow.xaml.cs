094ae3f8393ef739ad8638b92b121bc90e927a2c
Kangarion <mr.trushkoff2016@mail.ru>
2024-06-02 03:06:00 -0400

    Add new project to repositori.

---
+++ DemExam/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemExam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LogIn _logIn = new();
+        private Main _main = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainContent.Content = _logIn;
+            _logIn.LogInButton.Click += LogIn;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void LogIn(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = _main;
+        }
+    }
+}
